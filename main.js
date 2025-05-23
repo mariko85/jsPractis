fb9dc01723b17cd553b267a05a58d2ff8873b824
mariko85 <m.chaxvashvili@gmail.com>
2025-05-23 23:29:31 +0400

    // დავალება 6
    add array

--- main.js
+++ main.js
@@ -42,3 +42,20 @@ switch(true){
 
 
 
+
+            
+
+
+
+            // დავალება 6
+            
+const array = [10, 20, 30, 40, 50];
+const valueToFind = 31;
+
+if (array.includes(valueToFind)){
+console.log('რიცხვი ნაპოვნია')
+}
+else{
+ console.log('"რიცხვი არ მოიძებნა"')   
+
+}
